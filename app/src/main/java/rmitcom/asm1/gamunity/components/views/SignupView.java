39af114ab1f52ba385ead4e54d85af8d5258e082
toulletou03 <toulletou03@gmail.com>
2024-01-13 11:43:00 +0700

    Crash check

--- app/src/main/java/rmitcom/asm1/gamunity/components/views/SignupView.java
+++ app/src/main/java/rmitcom/asm1/gamunity/components/views/SignupView.java
@@ -1,16 +1,23 @@
 package rmitcom.asm1.gamunity.components.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 import rmitcom.asm1.gamunity.MainActivity;
@@ -19,7 +26,22 @@ import rmitcom.asm1.gamunity.model.User;
 
 public class SignupView extends AppCompatActivity {
     private EditText signupFName, signupLName, signupDOB, signupEmail, signupPassword;
+//    private CheckBox locationCheckbox;
 
+    private void showDatePicker() {
+        @SuppressLint("SetTextI18n") DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                (view, year, monthOfYear, dayOfMonth) -> {
+                    signupDOB.setText(dayOfMonth + "/" + (monthOfYear+1) + "/" + year);
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +52,19 @@ public class SignupView extends AppCompatActivity {
 
         signupFName = findViewById(R.id.signup_firstname);
         signupLName = findViewById(R.id.signup_lastname);
+
         signupDOB = findViewById(R.id.signup_birthday);
+        signupDOB.setInputType(InputType.TYPE_NULL); // disable soft input
+        signupDOB.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                showDatePicker();
+            }
+            return false;
+        });
+
         signupEmail = findViewById(R.id.signup_email);
         signupPassword = findViewById(R.id.signup_password);
+//        locationCheckbox = findViewById(R.id.location_checkbox);
 
         Button signupButton = findViewById(R.id.signup_button);
         TextView loginRedirectText = findViewById(R.id.login_redirect);
@@ -43,6 +75,7 @@ public class SignupView extends AppCompatActivity {
             String dob = signupDOB.getText().toString().trim();
             String email = signupEmail.getText().toString().trim();
             String pass = signupPassword.getText().toString().trim();
+//            boolean isLocated = locationCheckbox.isChecked();
 
             if (fName.isEmpty()){
                 signupFName.setError("First name cannot be empty");
@@ -67,7 +100,7 @@ public class SignupView extends AppCompatActivity {
 
                         // Create a User object with the provided details
                         User newUser = new User(userId, false, name, dob, email);
-
+//                        User newUser = new User(userId, false, name, dob, email, isLocated);
                         // Add the user to Firestore
                         db.collection("users").document(userId)
                                 .set(newUser)
