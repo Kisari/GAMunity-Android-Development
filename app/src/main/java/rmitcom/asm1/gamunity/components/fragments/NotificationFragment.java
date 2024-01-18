1412a8e1a23fce37aa70b43a1af5ed44d179de3f
Kisari <seikakukisari@gmail.com>
2024-01-18 16:43:52 +0700

    Notification pull-to-refresh to load the new notification

--- app/src/main/java/rmitcom/asm1/gamunity/components/fragments/NotificationFragment.java
+++ app/src/main/java/rmitcom/asm1/gamunity/components/fragments/NotificationFragment.java
@@ -9,6 +9,7 @@ import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +21,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -29,6 +31,7 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import rmitcom.asm1.gamunity.R;
 import rmitcom.asm1.gamunity.adapter.NotificationListAdapter;
@@ -43,6 +46,8 @@ public class NotificationFragment extends Fragment implements FirebaseFetchAndSe
     private final ArrayList<Notification> notificationArrayList = new ArrayList<>();
     private final FireBaseManager db = new FireBaseManager();
     private final Constant constant = new Constant();
+
+    private SwipeRefreshLayout swipeRefreshLayout;
     View currentView;
     private final ActivityResultLauncher<String> requestPermissionLauncher;
 
@@ -103,6 +108,12 @@ public class NotificationFragment extends Fragment implements FirebaseFetchAndSe
     }
 
     private void initializeNotification(){
+        swipeRefreshLayout = currentView.findViewById(R.id.refreshLayout);
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            recreate(requireActivity());
+        });
+
         db.getMsgProvider().getToken()
             .addOnCompleteListener(task -> {
                 if(task.isSuccessful()){
@@ -163,6 +174,8 @@ public class NotificationFragment extends Fragment implements FirebaseFetchAndSe
             Log.d(TAG, "Navigate to notification: " + id);
             updateTheReadNotification(position);
         });
+
+        swipeRefreshLayout.setRefreshing(false);
     }
 
 
