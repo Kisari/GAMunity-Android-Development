07ce742b39a88c44c5712247d27bc490e7ca52f8
Kisari <seikakukisari@gmail.com>
2024-01-20 12:29:29 +0700

    update

---
+++ app/src/main/java/rmitcom/asm1/gamunity/components/views/profile/EditProfileView.java
@@ -0,0 +1,14 @@
+package rmitcom.asm1.gamunity.components.views.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import rmitcom.asm1.gamunity.R;
+
+public class EditProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_view);
+    }
+}
