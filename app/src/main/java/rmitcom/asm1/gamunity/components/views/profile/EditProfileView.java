8f3df75f17f61f5c0fcef9d1e9c118f22b994bb3
Kisari <seikakukisari@gmail.com>
2024-01-21 19:31:45 +0700

    final commit

--- app/src/main/java/rmitcom/asm1/gamunity/components/views/profile/EditProfileView.java
+++ app/src/main/java/rmitcom/asm1/gamunity/components/views/profile/EditProfileView.java
@@ -2,53 +2,53 @@ package rmitcom.asm1.gamunity.components.views.profile;
 
 import static android.content.ContentValues.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import rmitcom.asm1.gamunity.R;
 import rmitcom.asm1.gamunity.components.ui.AsyncImage;
-import rmitcom.asm1.gamunity.components.views.forum.CreateForumView;
+import rmitcom.asm1.gamunity.components.views.LoginView;
 import rmitcom.asm1.gamunity.db.FireBaseManager;
 import rmitcom.asm1.gamunity.helper.FirebaseFetchAndSetUI;
 import rmitcom.asm1.gamunity.model.Constant;
-import rmitcom.asm1.gamunity.model.Forum;
 import rmitcom.asm1.gamunity.model.User;
 
 public class EditProfileView extends AppCompatActivity implements FirebaseFetchAndSetUI {
 
-    private FireBaseManager db = new FireBaseManager();
-    private Constant constant = new Constant();
+    private final FireBaseManager db = new FireBaseManager();
+    private final Constant constant = new Constant();
     private EditText userFirstName, userLastName, userBirth;
     private ImageView userBackground, userBackgroundBtn, editProfileSubmitButton, returnBack;
     private ImageButton userIconBtn;
+    private Button deleteButton;
     private ProgressBar backgroundProgress, iconProgress;
     private Uri backgroundFilePath;
     private String userDocumentRef;
@@ -70,6 +70,7 @@ public class EditProfileView extends AppCompatActivity implements FirebaseFetchA
         iconProgress = findViewById(R.id.progress2);
         editProfileSubmitButton = findViewById(R.id.editProfileSubmitButton);
         returnBack = findViewById(R.id.returnBack);
+        deleteButton = findViewById(R.id.delete_button);
 
 
 
@@ -82,7 +83,44 @@ public class EditProfileView extends AppCompatActivity implements FirebaseFetchA
             finish();
         });
 
+        deleteButton.setOnClickListener(v -> deleteAccount());
+
         fetchData();
+    }
+
+    private void deleteAccount() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditProfileView.this);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View deleteDialogLayout = inflater.inflate(R.layout.ui_delete_dialog_view, null);
+
+        TextView dialogMessage = deleteDialogLayout.findViewById(R.id.dialogMessage);
+        Button cancelButton = deleteDialogLayout.findViewById(R.id.dialogCancel);
+        Button deleteButton = deleteDialogLayout.findViewById(R.id.dialogAccept);
+
+        builder.setView(deleteDialogLayout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        try {
+            dialogMessage.setText("Are you sure you want delete this account ? This can not be reverted");
+            cancelButton.setOnClickListener(v -> dialog.dismiss());
+
+            deleteButton.setOnClickListener(v -> db.getCurrentUser().delete()
+                .addOnCompleteListener(task -> {
+                   if(task.isSuccessful()){
+                       Toast.makeText(EditProfileView.this, "Delete the account", Toast.LENGTH_SHORT).show();
+                       dialog.dismiss();
+
+                       Intent loginIntent = new Intent(EditProfileView.this, LoginView.class);
+                       startActivity(loginIntent);
+                       finish();
+                   }
+                }));
+
+        } catch (Exception e) {
+            Log.e("Forum", "getView: ", e);
+            e.printStackTrace();
+        }
     }
 
     private void chooseImageFromFile(Boolean isBackground){
